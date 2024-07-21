5cb33c2d0542ac80916c3df939bbfef1e13c7875
Nathan Leiby <nathanleiby@gmail.com>
2024-07-20 19:23:30 -0700

    add dev tools. show FPS

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
 
 use bevy::asset::AssetMetaCheck;
 use bevy::prelude::*;
+use bevy_dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin};
 
 fn main() {
     App::new()
@@ -16,6 +17,18 @@ fn main() {
             meta_check: AssetMetaCheck::Never,
             ..default()
         }))
+        .add_plugins(FpsOverlayPlugin {
+            config: FpsOverlayConfig {
+                text_config: TextStyle {
+                    // Here we define size of our overlay
+                    font_size: 50.0,
+                    // We can also change color of the overlay
+                    color: Color::srgb(0.0, 1.0, 0.0),
+                    // If we want, we can use a custom font
+                    font: default(),
+                },
+            },
+        })
         .add_systems(Startup, setup)
         .add_systems(Update, sprite_movement)
         .run();
