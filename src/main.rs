561f593655ee9ecb201661e1dff90367ab659cdb
Nathan Leiby <nathanleiby@gmail.com>
2024-07-20 19:19:44 -0700

    animate sprite movement
    https://bevyengine.org/examples/2d-rendering/move-sprite/

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,40 @@ fn main() {
             ..default()
         }))
         .add_systems(Startup, setup)
+        .add_systems(Update, sprite_movement)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("ducky.png"),
-        ..Default::default()
-    });
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("ducky.png"),
+            ..Default::default()
+        },
+        Direction::Up,
+    ));
+}
+
+#[derive(Component)]
+enum Direction {
+    Up,
+    Down,
+}
+
+/// The sprite is animated by changing its translation depending on the time that has passed since
+/// the last frame.
+fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
+    for (mut logo, mut transform) in &mut sprite_position {
+        match *logo {
+            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
+            Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
+        }
+
+        if transform.translation.y > 200. {
+            *logo = Direction::Down;
+        } else if transform.translation.y < -200. {
+            *logo = Direction::Up;
+        }
+    }
 }
